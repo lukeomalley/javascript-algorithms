81e4a06ed5a6a0e4444e81bd1e98d66141f74748
Luke O'Malley <Lukeomalley95@gmail.com>
2019-10-09 22:10:26 -0400

    solved getNthFib and re-organized folder structure to order by difficulty

---
+++ easy/depthFirstSearch.js
@@ -0,0 +1,25 @@
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  depthFirstSearch(array) {
+    array.push(this.name);
+    for (let child of this.children) {
+      child.depthFirstSearch(array);
+    }
+    return array;
+  }
+}
+
+const test1 = new Node('A');
+test1.addChild('B').addChild('C');
+test1.children[0].addChild('D');
+
+console.log(test1.depthFirstSearch([]));
