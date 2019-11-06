5e3c4b829a7b48579d00ac1857e9c2a5963431b4
Luke O'Malley <Lukeomalley95@gmail.com>
2019-11-06 10:12:07 -0500

    added and solved sumOfPairs

--- codewars/sumOfPairs.js
+++ codewars/sumOfPairs.js
@@ -33,3 +33,20 @@ const sumPairs = (array, targetSum) => {
 };
 
 sumPairs([10, 5, 2, 3, 7, 5], 10);
+
+var sum_pairs = function(ints, s) {
+  if (ints.length < 2) return undefined; //not enough numbers for pair.
+  let intSet = new Set();
+  intSet.add(ints[0]);
+  for (let i = 1; i < ints.length; ++i) {
+    let needed = s - ints[i];
+    if (intSet.has(needed)) {
+      //check if we have already seen the number needed to complete the pair.
+      return [needed, ints[i]];
+    }
+    intSet.add(ints[i]); //if not insert the number in set and continue.
+  }
+  return undefined; //No answer found
+};
+
+console.log(sum_pairs([10, 5, 2, 3, 7, 5], 10));
