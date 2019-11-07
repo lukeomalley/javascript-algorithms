b5e2145041c1d733321e4e6188e5e05317bfbe31
Luke O'Malley <Lukeomalley95@gmail.com>
2019-11-07 10:47:58 -0500

    solved longestPalindromicSubstring

---
+++ medium/longestPalindromicSubstring.js
@@ -0,0 +1,22 @@
+function longestPalindromicSubstring(string) {
+  let currentLongest = [0, 1];
+  for (let i = 1; i < string.length; i++) {
+    const odd = getLongestPalindromeFrom(string, i - 1, i + 1);
+    const even = getLongestPalindromeFrom(string, i - 1, i);
+    const longest = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+    currentLongest =
+      longest[1] - longest[0] > currentLongest[1] - currentLongest[0] ? longest : currentLongest;
+  }
+  return string.slice(currentLongest[0], currentLongest[1]);
+}
+
+function getLongestPalindromeFrom(string, leftIdx, rightIdx) {
+  while (leftIdx >= 0 && rightIdx < string.length) {
+    if (string[leftIdx] !== string[rightIdx]) {
+      break;
+    }
+    leftIdx -= 1;
+    rightIdx += 1;
+  }
+  return [leftIdx + 1, rightIdx];
+}
